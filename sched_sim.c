1968fa96e86b979b7c20250d70055e9f99ac459f
Alessandro Calandrelli <calandrelli.1846459@studenti.uniroma1.it>
2021-09-21 12:15:49 +0200

    SRJF implementato (trucchetto del quanto a 1), magari rifare test per verificare corretezza scheduler..pensare a nuove politiche (prio?) e aggiugnere commenti descrittivi

--- sched_sim.c
+++ sched_sim.c
@@ -19,62 +19,31 @@ void schedRR(FakeOS* os, void* args_){
   // if none, return
   if (!os->ready.first)
     return;
-
-  FakePCB* pcb=(FakePCB*) List_popFront(&os->ready);
-  
-  //Se c'è spazio nei core inserisco in running il pcb appena preso dai ready
-  if (List_isFull(&os->running)==0){
-    List_pushBack(&os->running,(ListItem*)pcb);  
-  }
-  else return;
-  
-  assert(pcb->events.first);
-  ProcessEvent* e = (ProcessEvent*)pcb->events.first;
-  assert(e->type==CPU);
-
-  // look at the first event
-  // if duration>quantum
-  // push front in the list of event a CPU event of duration quantum
-  // alter the duration of the old event subtracting quantum
-  if (e->duration>args->quantum) {
-    ProcessEvent* qe=(ProcessEvent*)malloc(sizeof(ProcessEvent));
-    qe->list.prev=qe->list.next=0;
-    qe->type=CPU;
-    qe->duration=args->quantum;
-    e->duration-=args->quantum;
-    List_pushFront(&pcb->events, (ListItem*)qe);
-  }
-};
-
-void schedSRJF(FakeOS* os, void* args_){
-  SchedRRArgs* args=(SchedRRArgs*)args_;
-  
-  // look for the first process in ready
-  // if none, return
-  if (!os->ready.first)
-    return;
-
   FakePCB* pcb;
-  //FakePCB* pcb=(FakePCB*) List_popFront(&os->ready);
 
-  ListItem* aux = os->ready.first;
-  int min = 100;
-  FakePCB* minpcb;
-  int  concurrent;
+  //Se scheduler==1 allora dalla ready verrà preso il primo processo
+  if (scheduler==1) pcb = (FakePCB*) List_popFront(&os->ready);
 
-  while(aux){
-    pcb = (FakePCB*) aux;
-    ProcessEvent* evento = (ProcessEvent*) pcb->events.first;
-    concurrent = evento->duration;
-    if(concurrent<min) {
-      min = concurrent;
-      minpcb = pcb;
+  // Se scheduler == 2 allora l'utente ha scelto una politica SRJF quando il quanto termina, dalla ready verrà estratto il processo con meno tempo rimanente 
+  else if (scheduler == 2){
+    ListItem* aux = os->ready.first;
+    int min = 100;
+    FakePCB* minpcb;
+    int  concurrent;
+
+    while(aux){
+      pcb = (FakePCB*) aux;
+      ProcessEvent* evento = (ProcessEvent*) pcb->events.first;
+      concurrent = evento->duration;
+      if(concurrent<min) {
+        min = concurrent;
+        minpcb = pcb;
+      }
+      aux = aux->next; 
     }
-    aux = aux->next;
+    pcb = (FakePCB*) List_detach(&os->ready,(ListItem*) minpcb);
   }
-
-  pcb = (FakePCB*) List_detach(&os->ready,(ListItem*) minpcb);
-
+  
   //Se c'è spazio nei core inserisco in running il pcb appena preso dai ready
   if (List_isFull(&os->running)==0){
     List_pushBack(&os->running,(ListItem*)pcb);  
@@ -103,6 +72,8 @@ int main(int argc, char** argv) {
 
   //Prendo il numero di parametri...
 
+  
+
   while(nuclei<1){
     printf("Inserisci il numero di core voluti, minimo 1: ");
     scanf("%d",&nuclei);
@@ -110,19 +81,16 @@ int main(int argc, char** argv) {
   }
 
   while(scheduler<1 || scheduler >2){
-    printf("Inserisci il tipo di scheduler voluto: 1->RR 2->SRJF");
+    printf("Inserisci il tipo di scheduler voluto 1->RR 2->SRJF: ");
     scanf("%d",&scheduler);
   }
-  
 
   FakeOS_init(&os);
   SchedRRArgs srr_args;
-  srr_args.quantum=5;
+  if(scheduler == 2) srr_args.quantum=1;
+  else srr_args.quantum=5;
   os.schedule_args=&srr_args;
-  os.schedule_fn=schedRR;
-
-  
-  //os.schedule_fn=schedSRJF;
+  os.schedule_fn=schedRR; 
   
   for (int i=1; i<argc; ++i){
     FakeProcess new_process;

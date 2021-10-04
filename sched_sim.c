a0a8c607e1a52dd35b5b66e3ba3494c116473f72
Alessandro Calandrelli <calandrelli.1846459@studenti.uniroma1.it>
2021-10-04 17:20:28 +0200

    Iniziati i commenti da fare, per ora completato sched_sim.c

--- sched_sim.c
+++ sched_sim.c
@@ -20,60 +20,65 @@ void sched(FakeOS* os, void* args_){
   if (!os->ready.first)
     return;
   FakePCB* pcb;
-
+  switch(scheduler){
   //Se scheduler==1 allora dalla ready verrà preso il primo processo, classico round robin
-  if (scheduler==1) pcb = (FakePCB*) List_popFront(&os->ready);
+  case 1:
+  pcb = (FakePCB*) List_popFront(&os->ready);
+  break;
 
   // Se scheduler == 2 allora l'utente ha scelto una politica SRJF, dalla ready verrà estratto il processo con meno tempo rimanente <- può essere
   //invocato anche prima della fine del burst -> preemption
-  else if (scheduler == 2){
-    ListItem* aux = os->ready.first;
-    int min = 100;
-    FakePCB* minpcb;
-    int  concurrent;
+  case 2:
+    {
+      ListItem* aux = os->ready.first;
+      int min = 100;
+      FakePCB* minpcb;
+      int  concurrent;
 
-    while(aux){
-      pcb = (FakePCB*) aux;
-      ProcessEvent* evento = (ProcessEvent*) pcb->events.first;
-      concurrent = evento->duration;
-      if(concurrent<min) {
-        min = concurrent;
-        minpcb = pcb;
+      while(aux){
+        pcb = (FakePCB*) aux;
+        ProcessEvent* evento = (ProcessEvent*) pcb->events.first;
+        concurrent = evento->duration;
+        if(concurrent<min) {
+          min = concurrent;
+          minpcb = pcb;
+        }
+        aux = aux->next; 
       }
-      aux = aux->next; 
+      pcb = (FakePCB*) List_detach(&os->ready,(ListItem*) minpcb);
+      break;
     }
-    pcb = (FakePCB*) List_detach(&os->ready,(ListItem*) minpcb);
-  }
   // Se scheduler==3 allora l'utente ha scelto una politica di scheduling basata su priorità, verrà estratto dalla ready il processo con maggior priorità 
   // (valore numerico minore) <- può essere invocato anche prima della fine del burst -> preemption
-  else if(scheduler == 3){
-    ListItem* aux = os->ready.first;
-    int min = 100;
-    FakePCB* minpcb;
-    int  concurrent;
-    while(aux){
-      pcb = (FakePCB*) aux;
-      concurrent = pcb->temp_prio;
-      
-      //La priorità dei processi in attesa viene aumentata all'incirca ogni 4 burst (20 unità di tempo) finchè non raggiunge massima priorità o gli viene affidata la cpu,
-      //terminato il burst ogni processo torna ad avere la priorità assegnatagli all'inizio -> tutto per evitare starving dei processi a bassa priorità
-      //ogni volta che la priortà viene aumentata l'utente viene avvisato con un messaggio e il contatore viene azzerato
-      //printf("\naoooo MA NUCLEI =: %d\n",nuclei);
-      if (pcb->counter==20*nuclei) {
-        if(pcb->temp_prio>1) {
-          pcb->temp_prio--;
-          printf("\t\tprocess %d waited for %d time slots, his priority is now increased\n",pcb->pid,quanto-1);
-          pcb->counter=1;
+  case 3:
+    {
+      ListItem* aux = os->ready.first;
+      int min = 100;
+      FakePCB* minpcb;
+      int  concurrent;
+      while(aux){
+        pcb = (FakePCB*) aux;
+        concurrent = pcb->temp_prio;
+        //La priorità dei processi in attesa viene aumentata all'incirca ogni 4 burst (20 unità di tempo) finchè non raggiunge massima priorità o gli viene affidata la cpu,
+        //terminato il burst ogni processo torna ad avere la priorità assegnatagli all'inizio -> tutto per evitare starving dei processi a bassa priorità
+        //ogni volta che la priortà viene aumentata l'utente viene avvisato con un messaggio e il contatore viene azzerato
+        if (pcb->counter==20*nuclei) {
+          if(pcb->temp_prio>1) {
+            pcb->temp_prio--;
+            printf("\t\tprocess %d waited for %d time slots, his priority is now increased\n",pcb->pid,quanto-1);
+            pcb->counter=1;
+          }
         }
+        else pcb->counter++;
+        if(concurrent<=min) {
+          min = concurrent;
+          minpcb = pcb;
+        }
+        aux = aux->next; 
       }
-      else pcb->counter++;
-      if(concurrent<=min) {
-        min = concurrent;
-        minpcb = pcb;
-      }
-      aux = aux->next; 
+      pcb = (FakePCB*) List_detach(&os->ready,(ListItem*) minpcb);
+      break;
     }
-    pcb = (FakePCB*) List_detach(&os->ready,(ListItem*) minpcb);
   }
   
   //Se c'è spazio nei core inserisco in running il pcb appena preso dai ready
@@ -102,40 +107,26 @@ void sched(FakeOS* os, void* args_){
 
 int main(int argc, char** argv) {
 
-  //Prendo il numero di parametri...
-
-  
-  //QUANTI CORE VUOLE L'UTENTE?
-  /*swhile(nuclei<1){
-    printf("Inserisci il numero di core voluti, minimo 1: ");
-    scanf("%d",&nuclei);
-    if(nuclei==0) printf("Sul serio? Che ci fai con una CPU inutile?\n");
-  }
-  //CHE TIPO DI SCHEDULING VUOLE L'UTENTE?
-  while(scheduler<1 || scheduler >3){
-    printf("Inserisci il tipo di scheduler voluto 1->RR 2->SRJF 3->PRIO: ");
-    scanf("%d",&scheduler);
-  }*/
-
-  //QUANTO GRANDE IL QUANTO?
-  /*while(quanto<1){
-    printf("Inserisci la dimensione del quanto di tempo: ");
-    scanf("%d",&quanto);
-    if(nuclei==0) printf("Sul serio? Un quanto nullo, tanto valeva mettere 0 core.\n");
-  }*/
-
-  quanto = 5;
+  //Questa print è molto utile per far capire all'utente come eseguire il programma, l'ordine e il significato dei parametri da passargli
   printf("\n----------------------------------------------");
-  printf("\nPer l'esecuzione del programma è necessario far partire l'eseguibile passando come argomenti i seguenti: \n-Numero di core voluti(minimo 1) \n-Numero rappresentante la politica di scheduling: \n\t1->Round Robin\n\t2->Shortest Remaining Job First\n\t3->Priority Based(Assicurarsi di passare processi compatibili)\n-Processi da dare in pasto al programma\n");
+  printf("\nPer l'esecuzione del programma è necessario far partire l'eseguibile passando come argomenti i seguenti: \n-Numero di core voluti(minimo 1) \n-Numero rappresentante la politica di scheduling: \n\t1->Round Robin\n\t2->Shortest Remaining Job First\n\t3->Priority Based(Assicurarsi di passare processi compatibili) \n-Dimensione del quanto voluto, maggiore o uguale a 1 (suggerito 5)\n-Processi da dare in pasto al programma\n");
   printf("----------------------------------------------\n");
+  //Prendo il numero di core
   nuclei = atoi(argv[1]);
   if(nuclei<1){
     printf("\nSono stati richiesti meno di 1 core, questo è impossibile, ritenta...\n\n");
     return 0;
   }
+  //Prendo la politica di scheduling
   scheduler = atoi(argv[2]);
   if(scheduler<1 || scheduler >3){
     printf("\nNessuno scheduler è stato scelto, le opzioni dispondibili sono 1, 2, 3 guarda sopra...\n\n");
+    return 0;
+  }
+  //Prendo la grandezza del quanto
+  quanto = atoi(argv[3]);
+  if(quanto<1 ){
+    printf("\nDimensione del quanto non specificata o minore di 1, scegliere un intero positivo prego...\n\n");
     return 0;
   }
 
@@ -145,7 +136,7 @@ int main(int argc, char** argv) {
   os.schedule_args=&sched_args;
   os.schedule_fn=sched; 
   
-  for (int i=3; i<argc; ++i){
+  for (int i=4; i<argc; ++i){
     FakeProcess new_process;
     int num_events=FakeProcess_load(&new_process, argv[i]);
     printf("loading [%s], pid: %d, events:%d",
